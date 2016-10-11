a7fd525df6fb473039f0a82b09f50abebba86f19
Philipp Backes <philippbackes95@gmail.com>
2016-10-11 19:23:19 +0200

    Added Quaternstrings and MinProduct classes

---
+++ src/problemset2/MinProduct.java
@@ -0,0 +1,21 @@
+package problemset2;
+
+/**
+ * Exercise 1:
+ * 
+ * @author Philipp Backes, 191710
+ * @author Homa Alavi, 191720
+ * @author Jannis Scholz, 191481
+ *
+ */
+public class MinProduct {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
---
+++ src/problemset2/Quaternstrings.java
@@ -0,0 +1,21 @@
+package problemset2;
+
+/**
+ * Exercise 2:
+ * 
+ * @author Philipp Backes, 191710
+ * @author Homa Alavi, 191720
+ * @author Jannis Scholz, 191481
+ *
+ */
+public class Quaternstrings {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
