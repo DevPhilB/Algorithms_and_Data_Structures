a6fcbe911c1465314660eb50264b55f5327bf05b
Philipp Backes <philippbackes95@gmail.com>
2016-10-25 19:23:21 +0200

    Added Turns and PathNodes classes

---
+++ src/problemset4/PathNodes.java
@@ -0,0 +1,23 @@
+package problemset4;
+
+/**
+ * Program takes a natural number (n) from user input, 
+ * calculates the amount of all points on the path to this point (0, 0) -> (n, n) 
+ * and prints it out.
+ * 
+ * @author Philipp Backes, 191710
+ * @author Homa Alavi, 191720
+ * @author Jannis Scholz, 191481
+ *
+ */
+public class PathNodes {
+
+  /**
+   * Main method
+   * @param args
+   */
+  public static void main(String[] args) {
+    
+  }
+
+}
---
+++ src/problemset4/Turns.java
@@ -0,0 +1,23 @@
+package problemset4;
+
+/**
+ * Program takes a natural number (n) from user input, prints all travel options 
+ * between citys s0 to sn-1 (starts from s0 and ends in s0), 
+ * counts the travel options and print the amount out.
+ * Condition: The number of a following city has to be +/- >= 8 compared to the last city
+ * 
+ * @author Philipp Backes, 191710
+ * @author Homa Alavi, 191720
+ * @author Jannis Scholz, 191481
+ *
+ */
+public class Turns {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    
+  }
+
+}
